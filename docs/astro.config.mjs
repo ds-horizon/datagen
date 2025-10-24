65d5bdd015e4f9b8276c8d4bfa90d5cb3192f9d0
Rrutum Lavana <rrutum@dream11.com>
2025-10-24 15:39:51 +0530

    Nesting directories content changes

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -40,6 +40,7 @@ export default defineConfig({
                 'concepts/advanced/iter-variable',
                 'concepts/advanced/function-calls',
                 'concepts/advanced/model-references',
+                'concepts/advanced/nesting',
               ],
             },
             {
