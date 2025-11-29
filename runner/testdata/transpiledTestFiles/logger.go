d7262aebeca40842732690deac7d7baa592ed8c6
D11-nupur <nupur.gaur@dream11.com>
2025-11-29 15:05:02 +0530

    Revert "formatting done"
    This reverts commit c3bf33e4efa3d37e846a9167825a86c670dda772.

--- runner/testdata/transpiledTestFiles/logger.go
+++ runner/testdata/transpiledTestFiles/logger.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"log/slog"
-	"os"
-	"strings"
+    "context"
+    "fmt"
+    "io"
+    "log/slog"
+    "os"
+    "strings"
 )
 
 type __dgi_plainHandler struct {
-	w       io.Writer
-	leveler slog.Leveler
+    w       io.Writer
+    leveler slog.Leveler
 }
 
 func __dgi_NewPlainHandler(w io.Writer, opts *slog.HandlerOptions) *__dgi_plainHandler {
-	if opts == nil {
-		opts = &slog.HandlerOptions{}
-	}
-	return &__dgi_plainHandler{w: w, leveler: opts.Level}
+    if opts == nil {
+        opts = &slog.HandlerOptions{}
+    }
+    return &__dgi_plainHandler{w: w, leveler: opts.Level}
 }
 
 func (h *__dgi_plainHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	if h.leveler == nil {
-		return true
-	}
-	return level >= h.leveler.Level()
+    if h.leveler == nil {
+        return true
+    }
+    return level >= h.leveler.Level()
 }
 
 func (h *__dgi_plainHandler) Handle(ctx context.Context, r slog.Record) error {
-	ts := r.Time.Local().Format("2006-01-02T15:04:05")
-	lvl := strings.ToUpper(r.Level.String())
-	_, err := fmt.Fprintf(h.w, "%s %s %s\n", ts, lvl, r.Message)
-	return err
+    ts := r.Time.Local().Format("2006-01-02T15:04:05")
+    lvl := strings.ToUpper(r.Level.String())
+    _, err := fmt.Fprintf(h.w, "%s %s %s\n", ts, lvl, r.Message)
+    return err
 }
 
 func (h *__dgi_plainHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return h
+    return h
 }
 
 func (h *__dgi_plainHandler) WithGroup(name string) slog.Handler {
-	return h
+    return h
 }
 
 func __dgi_InitLogger(verbose bool) {
-	logLevel := slog.LevelInfo
-	if verbose {
-		logLevel = slog.LevelDebug
-	}
-	slog.SetDefault(slog.New(__dgi_NewPlainHandler(os.Stdout, &slog.HandlerOptions{
-		Level: logLevel,
-	})))
+    logLevel := slog.LevelInfo
+    if verbose {
+        logLevel = slog.LevelDebug
+    }
+    slog.SetDefault(slog.New(__dgi_NewPlainHandler(os.Stdout, &slog.HandlerOptions{
+            Level: logLevel,
+    })))
 }
